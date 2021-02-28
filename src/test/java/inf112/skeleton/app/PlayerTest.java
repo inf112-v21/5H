d230ca431aeb347d6a8faf4319feb5ee476aacc5
spectraldesign <59234024+spectraldesign@users.noreply.github.com>
2021-02-28 18:06:49 +0100

    Changed pathing for files.
    Signed-off-by: spectraldesign <59234024+spectraldesign@users.noreply.github.com>

--- src/test/java/inf112/skeleton/app/PlayerTest.java
+++ src/test/java/inf112/skeleton/app/PlayerTest.java
@@ -5,6 +5,8 @@ import inf112.skeleton.app.sprites.Player;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class PlayerTest {
