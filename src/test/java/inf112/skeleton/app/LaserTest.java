c4cba549f8893f0ec9341ad683d62ab3f22e4e6c
Jonas Svåsand <jonas.svasand@student.uib.no>
2021-03-25 22:26:16 +0100

    Removed errors from laser and collision test by giving game constructor option to not connect or create any server

--- src/test/java/inf112/skeleton/app/LaserTest.java
+++ src/test/java/inf112/skeleton/app/LaserTest.java
@@ -20,7 +20,7 @@ public class LaserTest {
 
 
     public void setUpBoard(int boardNum) {
-        NetworkSettings networkSettings = new NetworkSettings("server", "localhost", 2, 2);
+        NetworkSettings networkSettings = new NetworkSettings("test", "localhost", 2, 2);
         game = new Game(networkSettings, 4);
         board = new Board();
         board.readBoard(boardNum);
