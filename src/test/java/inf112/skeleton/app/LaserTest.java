f83c116b05c23f3fdc10af3f0e1a6ebc41ba6435
Jonas Svåsand <jonas.svasand@student.uib.no>
2021-04-06 15:40:51 +0200

    Added playerlasertest with board100, added createaliveplayerlist in game and used it in test + replaced in game.create(), fixed out of bounds error bug in fireLaseres

--- src/test/java/inf112/skeleton/app/LaserTest.java
+++ src/test/java/inf112/skeleton/app/LaserTest.java
@@ -14,6 +14,7 @@ public class LaserTest {
     private int player1PcBefore;
     private Player player2;
     private int player2PcBefore;
+    private Player player3;
     private int player4PcBefore;
     private Player player4;
     private Game game;
@@ -26,11 +27,13 @@ public class LaserTest {
         board = new Board();
         board.readBoard(boardNum);
         game.board = board;
+        game.createAlivePlayerList();
         game.setBoardSize(board.getSize());
         player1 = board.getPlayerList().get(0);
         player1PcBefore = player1.getPc();
         player2 = board.getPlayerList().get(1);
         player2PcBefore = player2.getPc();
+        player3 = board.getPlayerList().get(2);
         player4 = board.getPlayerList().get(3);
         player4PcBefore = player4.getPc();
         pcBefore = makePlayerPcHashMap();
@@ -45,6 +48,7 @@ public class LaserTest {
         return hashMap;
     }
 
+    // Board Laser tests
     @Test
     public void noPlayerTakesDamage() {
         setUpBoard(100);
@@ -59,20 +63,21 @@ public class LaserTest {
         //Make a hashmap with expected pc values based on pc values before
         HashMap<Player, Integer> expectedPc = pcBefore;
         // Player 1 is expected to take 1 damage so subtract 1 from player1s pc value
-        expectedPc.put(player1, expectedPc.get(player1)-1);
+        expectedPc.put(player1, expectedPc.get(player1) - 1);
         // Make new hashmap with current pc values
         HashMap<Player, Integer> pcAfter = makePlayerPcHashMap();
         assertEquals(expectedPc, pcAfter, "Some player does not have the pc that was expected after firing lasers on this board");
-        }
+    }
 
     @Test
     public void player1DoesNotTakeDamageThroughPlayer2() {
         setUpBoard(102);
         game.fireLasers(board.getLaserList());
         //Player 2 will take damage and stop the laser so it shouldn't hit player 1
-        assertEquals(player2PcBefore-1, player2.getPc(), "Laser did not hit or did too much damage to player2");
+        assertEquals(player2PcBefore - 1, player2.getPc(), "Laser did not hit or did too much damage to player2");
         assertEquals(player1PcBefore, player1.getPc(), "Player 1 took damage somehow when they shouldn't");
     }
+
     @Test
     public void wallPreventsDamage() {
         setUpBoard(103);
@@ -80,19 +85,39 @@ public class LaserTest {
         assertEquals(player1PcBefore, player1.getPc(), "Player 1 took damage through the wall!");
 
     }
+
     @Test
     public void moreThanOneLaserFiresInOneMethodCall() {
         setUpBoard(104);
         game.fireLasers(board.getLaserList());
-        assertEquals(player1PcBefore -1, player1.getPc(),"Laser 1 didn't fire"); //Note it will break here and you don't know if laser 2 fired
-        assertEquals(player4PcBefore -1, player4.getPc(), "Laser 2 didn't fire");
+        assertEquals(player1PcBefore - 1, player1.getPc(), "Laser 1 didn't fire"); //Note it will break here and you don't know if laser 2 fired
+        assertEquals(player4PcBefore - 1, player4.getPc(), "Laser 2 didn't fire");
     }
 
     @Test
     public void allFourLasersFireAndDamage() {
         setUpBoard(105);
         game.fireLasers(board.getLaserList());
-        assertEquals(player1PcBefore -4, player1.getPc());
+        assertEquals(player1PcBefore - 4, player1.getPc());
     }
 
+
+    //Player Laser tests:
+    @Test
+    public void player2and3and4TakesDamage() {
+        setUpBoard(100);
+        game.fireLasers(game.getPlayerLasers());
+        HashMap<Player, Integer> expectedPc = new HashMap<>();
+        expectedPc.put(player1, 9);
+        expectedPc.put(player2, 8);
+        expectedPc.put(player3, 8);
+        expectedPc.put(player4, 8);
+        assertEquals(expectedPc, makePlayerPcHashMap(), "Damage not as expected");
+
+
+
+
     }
+
+}
+
