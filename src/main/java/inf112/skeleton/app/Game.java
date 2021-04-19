e61669c92ef2bd92c4bb266552c82d39eea5941d
Jonas Svåsand <jonas.svasand@student.uib.no>
2021-04-19 21:28:03 +0200

    Forandret tall til å være sprites istedenfor text

--- src/main/java/inf112/skeleton/app/Game.java
+++ src/main/java/inf112/skeleton/app/Game.java
@@ -123,7 +123,6 @@ public class Game implements ApplicationListener {
         camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
         font = new BitmapFont();
         bgTexture = new Texture("src/main/resources/tex/background.png");
-
         Stage stage = new Stage();
         Gdx.input.setInputProcessor(stage);
 
@@ -154,6 +153,8 @@ public class Game implements ApplicationListener {
                 submittedCards = true;
             }
         });
+
+
     }
 
     /**
@@ -210,7 +211,11 @@ public class Game implements ApplicationListener {
         spriteMap.put("turnLeft", new Sprite(new Texture("src/main/resources/tex/cards/turnLeft.png")));
         spriteMap.put("turnRight", new Sprite(new Texture("src/main/resources/tex/cards/turnRight.png")));
         spriteMap.put("uTurn", new Sprite(new Texture("src/main/resources/tex/cards/uTurn.png")));
-
+        //Create sprites for showing score/hp/pc as text scales badly to the required size
+        for (int i = 0; i<10; i++) {
+            String textureString = "src/main/resources/numbers/" + i + ".png";
+            spriteMap.put(String.valueOf(i),new Sprite(new Texture(textureString)));
+        }
         createAlivePlayerList();
         boardSize = board.getSize();
     }
@@ -308,7 +313,7 @@ public class Game implements ApplicationListener {
             }
             phase = Phase.CARD_SELECT;
             submittedCards = false;
-            statusMessage = "Select cards to move. Click SUBMIT CARDS when ready (not implemented yet)";
+            statusMessage = "Select cards to move. Click SUBMIT CARDS when ready";
             gameServerListener.resetReceivedMoves();
         } else if (phase == Phase.CARD_SELECT) { //If player should choose cards
             if (hand.getNumberOfCardsSelected() != 5) { //If the player has not registered enough cards.
@@ -492,11 +497,22 @@ public class Game implements ApplicationListener {
         drawPlayer.setSize(68, 68);
         drawPlayer.draw(batch); //Draw player object in the GUI
 
-        //Draw HP and PC
-        font.getData().setScale(5); //Size up font so its readable
-        font.draw(batch, "" + thisPlayer.getHp(), 444, 709);
-        font.draw(batch, "" + thisPlayer.getPc(), 574, 709);
-        font.draw(batch, "" + thisPlayer.getScore(), 990, 125);
+        // Put correct hp/pc/score sprites onto board
+        Sprite hpSprite = spriteMap.get(String.valueOf(thisPlayer.getHp()));
+        hpSprite.setPosition(430, 650);
+        hpSprite.setSize(60,60);
+        hpSprite.draw(batch);
+
+        Sprite pcSprite = spriteMap.get(String.valueOf(thisPlayer.getPc()));
+        pcSprite.setPosition(565, 650);
+        pcSprite.setSize(60,60);
+        pcSprite.draw(batch);
+
+        Sprite scoreSprite = spriteMap.get(String.valueOf(thisPlayer.getScore()));
+        scoreSprite.setPosition(965, 68);
+        scoreSprite.setSize(60,60);
+        scoreSprite.draw(batch);
+
     }
 
     /**
